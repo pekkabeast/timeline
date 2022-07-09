c29eb7d0d7dc9575e5adf5815d5ff1b8915c9ffa
Victor <vctrchn@hotmail.ca>
2022-07-09 19:23:27 -0400

    creating page template

---
+++ script.js
@@ -0,0 +1,26 @@
+class cardName extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+
+        )
+    }
+}
+
+class playerCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='card'>
+
+            </div>
+        )
+    }
+
+
+}
